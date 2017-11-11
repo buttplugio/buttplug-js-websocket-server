0cafb0863fc7b9de5bc393cb6aa88d3602841a95
Kyle Machulis <kyle@nonpolynomial.com>
2017-11-10 23:42:49 -0800

    feat: Add command line args and ssl/self signing
    Add ability to use command line args, and generate self signed
    certificates to use for secure websockets.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
+import * as commander from "commander";
 import * as ws from "ws";
+import * as https from "https";
 import { ButtplugServer, ButtplugMessage, FromJSON } from "buttplug";
 import { NobleBluetoothDeviceManager } from "./NobleBluetoothDeviceManager";
+const selfsigned = require("selfsigned");
+
+const attrs = [{
+  name: "commonName",
+  value: "buttplug.localhost",
+}, {
+  name: "organizationName",
+  value: "Metafetish",
+}];
+
+const pems = selfsigned.generate(undefined, { days: 365 });
+console.log(pems);
+
+const server = https.createServer({
+  cert: pems.cert,
+  key: pems.private,
+}).listen(12345);
+
+commander
+  .version("0.0.1")
+  .option("-h, --host", "host address to listen on, defaults to localhost.", "localhost")
+  .option("-p, --port", "port to listen on, defaults to 12345", 12345)
+  .parse(process.argv);
 
 process.on("unhandledRejection", (reason, p) => {
   console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
@@ -10,7 +35,7 @@ process.on("unhandledRejection", (reason, p) => {
 const bs = new ButtplugServer();
 bs.AddDeviceManager(new NobleBluetoothDeviceManager());
 
-const wss = new ws.Server({ port: 12345 });
+const wss = new ws.Server({server});
 
 wss.on("connection", function connection(client) {
   client.on("message", async (message) => {
