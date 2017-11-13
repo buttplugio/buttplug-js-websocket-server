607b66ddb569bf72a35999e2c6f6428ac7a90c71
Kyle Machulis <kyle@nonpolynomial.com>
2017-11-12 17:21:47 -0800

    feat: Add ability to choose between secure/insecure websocket connections

--- src/index.ts
+++ src/index.ts
@@ -3,55 +3,64 @@ import * as ws from "ws";
 import * as fs from "fs";
 import * as util from "util";
 import * as https from "https";
+import * as http from "http";
 import { ButtplugServer, ButtplugMessage, FromJSON } from "buttplug";
 import { NobleBluetoothDeviceManager } from "./NobleBluetoothDeviceManager";
 const selfsigned = require("selfsigned");
 
 async function main() {
-  const attrs = [{
-    name: "commonName",
-    value: "buttplug.localhost",
-  }, {
-    name: "organizationName",
-    value: "Metafetish",
-  }];
-
-  let pems: any = {};
-  const existsAsync = util.promisify(fs.exists);
-
-  if (await existsAsync("cert.pem") && await existsAsync("private.pem")) {
-    console.log("Loading keys");
-    pems.cert = fs.readFileSync("cert.pem");
-    pems.private = fs.readFileSync("private.pem");
-  } else {
-    console.log("Creating keys");
-    pems = selfsigned.generate(undefined, { days: 365 });
-    fs.writeFileSync("cert.pem", pems.cert);
-    fs.writeFileSync("private.pem", pems.private);
-  }
-
-  const server = https.createServer({
-    cert: pems.cert,
-    key: pems.private,
-  }).listen(12345);
-
-  commander
-    .version("0.0.1")
-    .option("-h, --host", "host address to listen on, defaults to localhost.", "localhost")
-    .option("-p, --port", "port to listen on, defaults to 12345", 12345)
-    .parse(process.argv);
-
   process.on("unhandledRejection", (reason, p) => {
     console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
     // application specific logging, throwing an error, or other logic here
   });
 
+  commander
+    .version("0.0.1-alpha")
+    .option("-p, --port <number>", "Port to listen on, defaults to 12345.", 12345)
+    .option("--nossl", "If passed, do not use SSL. Needed for ScriptPlayer. SSL on by default otherwise.", false)
+    .parse(process.argv);
+
+  let wsServer;
+  if (commander.nossl) {
+    console.log("Not using SSL.");
+    const server = http.createServer().listen(commander.port);
+    wsServer = new ws.Server({server});
+  } else {
+    console.log("Using SSL.");
+    const attrs = [{
+      name: "commonName",
+      value: "buttplug.localhost",
+    }, {
+      name: "organizationName",
+      value: "Metafetish",
+    }];
+
+    let pems: any = {};
+    const existsAsync = util.promisify(fs.exists);
+
+    if (await existsAsync("cert.pem") && await existsAsync("private.pem")) {
+      console.log("Loading keys");
+      pems.cert = fs.readFileSync("cert.pem");
+      pems.private = fs.readFileSync("private.pem");
+    } else {
+      console.log("Creating keys");
+      pems = selfsigned.generate(undefined, { days: 365 });
+      fs.writeFileSync("cert.pem", pems.cert);
+      fs.writeFileSync("private.pem", pems.private);
+    }
+
+    const server = https.createServer({
+      cert: pems.cert,
+      key: pems.private,
+    }).listen(commander.port);
+    wsServer = new ws.Server({server});
+  }
+
+  console.log("Listening on port " + commander.port);
   const bs = new ButtplugServer();
   bs.AddDeviceManager(new NobleBluetoothDeviceManager());
 
-  const wss = new ws.Server({server});
-
-  wss.on("connection", function connection(client) {
+  wsServer.on("connection", function connection(client) {
     client.on("message", async (message) => {
       const msg = FromJSON(message);
       for (const m of msg) {
@@ -64,7 +73,6 @@ async function main() {
       client.send("[" + message.toJSON() + "]");
     });
   });
-
 }
 
 main();
